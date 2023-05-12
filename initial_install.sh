26eb9c6528f3299a993b8a2a5fce848b86e2dce2
takoyaki65 <takoyaki65@users.noreply.github.com>
2023-05-12 17:23:58 +0900

    update fish configuration

--- initial_install.sh
+++ initial_install.sh
@@ -9,9 +9,29 @@ sudo apt install libgtest-dev -y
 sudo apt install fish -y
 chsh -s /usr/bin/fish
 
+# install fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+# install oh-my-fish/theme-bobthefish
+fisher install oh-my-fish/theme-bobthefish
+
+# install HackGen_v2.9.0
+wget https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_v2.9.0.zip
+unzip HackGen_v2.9.0.zip
+sudo mkdir -p /usr/share/fonts/truetype/HackGen
+sudo cp HackGen_v2.9.0/*.ttf /usr/share/fonts/truetype/HackGen
+sudo fc-cache -fv
+
 # install trash-cli
 sudo apt install trash-cli -y
 
 # Gitの設定
 git config --global user.name "takoyaki65"
 git config --global user.email "takoyaki65@users.noreply.github.com"
+
+# initialize .bashrc
+rm ~/.bashrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+
+#initialize fish.config
+rm ~/.config/fish/config.fish
+ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
