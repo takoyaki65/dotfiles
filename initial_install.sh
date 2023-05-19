211ca5c96694e0369b063b85ed0003a8bc344403
takoyaki65 <takoyaki65@users.noreply.github.com>
2023-05-19 14:26:27 +0900

    disable fisher and oh-my-fish/theme-bobthefish

--- initial_install.sh
+++ initial_install.sh
@@ -7,12 +7,14 @@ sudo apt install texlive-full -y
 sudo apt install libgtest-dev -y
 # install fish shell
 sudo apt install fish -y
-chsh -s /usr/bin/fish
+# set fish as default shell
+#chsh -s /usr/bin/fish
 
 # install fisher
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+#curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 # install oh-my-fish/theme-bobthefish
-fisher install oh-my-fish/theme-bobthefish
+# theme-bobthefish makes fish slow, so I don't use it now.
+#fisher install oh-my-fish/theme-bobthefish
 
 # install HackGen_v2.9.0
 wget https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_v2.9.0.zip
