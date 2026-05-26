e4f12fce668cc6fd12c678b471b0562984e219d2
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2026-05-26 23:12:00 +0900

    fix init script

--- nix/modules/home/programs/bash.nix
+++ nix/modules/home/programs/bash.nix
@@ -4,13 +4,12 @@
   programs.bash = {
     enable = true;
     # Bash is the default login shell (set by chsh on /etc/passwd).
-    # But we want to use fish shell, so we add initial scripts to load fish shell.
+    # But we want to use fish shell for login shells, so we replace login bash with fish.
     # ref: https://nixos.wiki/wiki/Fish#Setting_fish_as_your_shell
     initExtra = ''
-      if [[ $(ps -p $PPID -o comm=) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      if [[ $(ps -p $PPID -o comm=) != "fish" && -z ''${BASH_EXECUTION_STRING} ]] && shopt -q login_shell
       then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec fish $LOGIN_OPTION
+        exec fish --login
       fi
     '';
   };
