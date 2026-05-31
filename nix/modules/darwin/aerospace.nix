4b6a3a14da8a226706f4725e8b97418701e4ce97
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2026-05-31 17:10:06 +0900

    conf: Aerospace conf

--- nix/modules/darwin/aerospace.nix
+++ nix/modules/darwin/aerospace.nix
@@ -8,6 +8,7 @@
       keepAlive = true;
     };
 
+    # Based on: https://nikitabobko.github.io/AeroSpace/guide#default-config
     settings = {
       # Config version for compatibility and deprecations
       # Fallback value (if you omit the key): config-version = 1
@@ -148,6 +149,28 @@
             alt-tab = "workspace-back-and-forth";
             # See: https://nikitabobko.github.io/AeroSpace/commands#move-workspace-to-monitor
             alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
+
+            # See: https://nikitabobko.github.io/AeroSpace/commands#mode
+            alt-shift-semicolon = "mode service";
+          };
+        };
+
+        # 'service' binding mode declaration.
+        # See: https://nikitabobko.github.io/AeroSpace/guide#binding-modes
+        service = {
+          binding = {
+            esc = ["reload-config" "mode main"];
+            r = ["flatten-workspace-tree" "mode main"]; # reset-layout
+            f = ["layout floating tiling" "mode main"]; # Toggle between floating and tiling layout
+            backspace = ["close-all-windows-but-current" "mode main"];
+
+            # sticky is not yet supported https://github.com/nikitabobko/AeroSpace/issues/2
+            #s = ['layout sticky tiling', 'mode main']
+
+            alt-shift-h = ["join-with left" "mode main"];
+            alt-shift-j = ["join-with down" "mode main"];
+            alt-shift-k = ["join-with  up" "mode main"];
+            alt-shift-l = ["join-with right" "mode main"];
           };
         };
       };
