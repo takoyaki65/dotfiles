9d174ff2f382e47906ee6db539571ca4cbf7b601
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2026-05-31 12:00:59 +0900

    Update nixpkgs, home-manager and nix-darwin from 25.11 to 26.05

--- flake.nix
+++ flake.nix
@@ -10,18 +10,18 @@
     # Pin our primary nixpkgs repository. This is the main nixpkgs repository
     # we'll use for our configurations. Be very careful changing this because
     # it'll impact your entire system.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
     # We use the unstable nixpkgs repo for some packages.
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -76,8 +76,6 @@
                 ./nix/modules/home
                 # User spaecific package
                 ./nix/modules/darwin/packages.nix
-                # User specific settings for Aerospace window manager
-                ./nix/modules/darwin/aerospace.nix
               ];
             };
             home-manager.extraSpecialArgs = {
