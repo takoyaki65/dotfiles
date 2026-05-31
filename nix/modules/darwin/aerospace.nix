c407625fa7fd94e58fc4c3a6685a01d301678151
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2026-05-31 15:43:04 +0900

    conf: Aerospace conf

--- nix/modules/darwin/aerospace.nix
+++ nix/modules/darwin/aerospace.nix
@@ -11,18 +11,24 @@
       # Start Aerospace at login
       start-at-login = true;
       gaps = {
-        outer.left = 10;
-        outer.bottom = 10;
-        outer.top = 10;
-        outer.right = 10;
+        outer.left = 8;
+        outer.bottom = 8;
+        outer.top = 8;
+        outer.right = 8;
       };
       mode = {
         main = {
           binding = {
+            # Focus window
             alt-h = "focus left";
             alt-j = "focus down";
             alt-k = "focus up";
             alt-l = "focus right";
+            # Move window
+            alt-shift-h = "move left";
+            alt-shift-j = "move down";
+            alt-shift-k = "move up";
+            alt-shift-l = "move right";
           };
         };
       };
