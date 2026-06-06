7116ca897c2658eb529c5779af406f06d19942e3
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2026-06-06 18:45:30 +0900

    nvim: Change settings for indentation

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -13,8 +13,8 @@ opt.number = true
 opt.relativenumber = true
 
 -- Indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.smartindent = true
 opt.autoindent = true
