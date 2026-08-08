90f5156afb8ef4bea62547a7b4696a27f823695a
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2026-08-08 18:28:24 +0900

    fix truecolor problem

--- nix/modules/linux/default.nix
+++ nix/modules/linux/default.nix
@@ -8,5 +8,11 @@
   # SSH preserves Ghostty's TERM value, but remote ncurses applications need
   # Ghostty's terminfo database on their search path to interpret it.
   # The trailing colon keeps ncurses' default system terminfo directories.
-  home.sessionVariables.TERMINFO_DIRS = "${pkgs.ghostty.terminfo}/share/terminfo:";
+  home.sessionVariables = {
+    TERMINFO_DIRS = "${pkgs.ghostty.terminfo}/share/terminfo:";
+
+    # Unlike TERM, SSH does not transmit COLORTERM as part of PTY allocation.
+    # This Linux profile uses Ghostty, which supports true color.
+    COLORTERM = "truecolor";
+  };
 }
