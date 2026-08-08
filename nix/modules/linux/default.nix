fcbada436f8eedba98aec6d435722e5aad80373c
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2026-08-08 18:12:14 +0900

    add terminfo settings of ghostty for linux

--- nix/modules/linux/default.nix
+++ nix/modules/linux/default.nix
@@ -4,4 +4,9 @@
   home.packages = with pkgs; [
     xclip
   ];
+
+  # SSH preserves Ghostty's TERM value, but remote ncurses applications need
+  # Ghostty's terminfo database on their search path to interpret it.
+  # The trailing colon keeps ncurses' default system terminfo directories.
+  home.sessionVariables.TERMINFO_DIRS = "${pkgs.ghostty.terminfo}/share/terminfo:";
 }
