a07cf149808fa036890b9a004df9dd94f333986f
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2026-08-22 20:29:05 +0900

    add wrapper for utmctl

--- nix/modules/darwin/packages.nix
+++ nix/modules/darwin/packages.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }:
 
+let
+  utmctl = pkgs.writeShellScriptBin "utmctl" ''
+    exec /Applications/UTM.app/Contents/MacOS/utmctl "$@"
+  '';
+in
+
 {
   # macOS-specific Nix packages (home-manager)
   home.packages = with pkgs; [
     mas
     keycastr
+    utmctl
   ];
 }
